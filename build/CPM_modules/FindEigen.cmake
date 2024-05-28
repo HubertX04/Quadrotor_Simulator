393b653135b4659a80cd99aad544ce24dd2fa97a
Hubert K. <hubert.kowzsnr@gmail.com>
2024-05-28 11:32:28 +0200

    First Commit

---
+++ build/CPM_modules/FindEigen.cmake
@@ -0,0 +1,3 @@
+include("C:/Users/huber/Desktop/projekt4/build/cmake/CPM_0.38.1.cmake")
+CPMAddPackage("NAME;Eigen;VERSION;3.4.0;URL;https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz;DOWNLOAD_ONLY;YES")
+set(Eigen_FOUND TRUE)
---
+++ build/SDL/include/SDL_revision.h
@@ -0,0 +1,6 @@
+#define SDL_REVISION "https://github.com/marekjg/tp_projekt_4@295d2337809a02136c9c11430e152c5ddb508443"
+#define SDL_REVISION_NUMBER 0
+
+#ifndef SDL_REVISION
+#define SDL_REVISION ""
+#endif
