eec8c7bd2ae2555b84f7fa6e7d9b6aeffd5b0e2d
Hubert K. <hubert.kowzsnr@gmail.com>
2024-05-29 13:18:01 +0200

    Ver.: 2.0 - add full-version simulation.cpp

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -2,6 +2,8 @@
  * SDL window creation adapted from https://github.com/isJuhn/DoublePendulum
 */
 #include "simulate.h"
+#include <thread>
+#include <matplot/matplot.h>
 
 Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     /* Calculate LQR gain matrix */
@@ -15,9 +17,9 @@ Eigen::MatrixXf LQR(PlanarQuadrotor &quadrotor, float dt) {
     Eigen::MatrixXf K = Eigen::MatrixXf::Zero(6, 6);
     Eigen::Vector2f input = quadrotor.GravityCompInput();
 
-    Q.diagonal() << 10, 10, 10, 1, 10, 0.25 / 2 / M_PI;
-    R.row(0) << 0.1, 0.05;
-    R.row(1) << 0.05, 0.1;
+    Q.diagonal() << 0.004, 0.004, 400, 0.008, 0.045, 2 / 2 / M_PI;
+    R.row(0) << 30, 7;
+    R.row(1) << 7, 30;
 
     std::tie(A, B) = quadrotor.Linearize();
     A_discrete = Eye + dt * A;
@@ -30,6 +32,20 @@ void control(PlanarQuadrotor &quadrotor, const Eigen::MatrixXf &K) {
     Eigen::Vector2f input = quadrotor.GravityCompInput();
     quadrotor.SetInput(input - K * quadrotor.GetControlState());
 }
+void doPlot(std::vector<float> x_history,std::vector<float> y_history,std::vector<float> theta_history,std::vector<float> time){
+
+    matplot::subplot(3,1,0);
+    matplot::plot(time,x_history);
+    matplot::title("X");
+    matplot::subplot(3,1,1);
+    matplot::plot(time,y_history);
+    matplot::title("Y");
+    matplot::subplot(3,1,2);
+    matplot::plot(time,theta_history);
+    matplot::title("Theta");
+    matplot::show();
+    
+}
 
 int main(int argc, char* args[])
 {
@@ -37,14 +53,12 @@ int main(int argc, char* args[])
     std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
     const int SCREEN_WIDTH = 1280;
     const int SCREEN_HEIGHT = 720;
+    const int start_x=350;
+    const int start_y=350;
 
-    /**
-     * TODO: Extend simulation
-     * 1. Set goal state of the mouse when clicking left mouse button (transform the coordinates to the quadrotor world! see visualizer TODO list)
-     *    [x, y, 0, 0, 0, 0]
-     * 2. Update PlanarQuadrotor from simulation when goal is changed
-    */
+
     Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
+    initial_state << start_x, start_y, 0, 0, 0, 0; 
     PlanarQuadrotor quadrotor(initial_state);
     PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
     /**
@@ -53,21 +67,18 @@ int main(int argc, char* args[])
      * For implemented LQR controller, it has to be [x, y, 0, 0, 0, 0]
     */
     Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
-    goal_state << -1, 7, 0, 0, 0, 0;
+    goal_state << SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0, 0, 0;
     quadrotor.SetGoal(goal_state);
     /* Timestep for the simulation */
-    const float dt = 0.001;
+    const float dt = 0.003;
     Eigen::MatrixXf K = LQR(quadrotor, dt);
     Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
 
-    /**
-     * TODO: Plot x, y, theta over time
-     * 1. Update x, y, theta history vectors to store trajectory of the quadrotor
-     * 2. Plot trajectory using matplot++ when key 'p' is clicked
-    */
-    std::vector<float> x_history;
-    std::vector<float> y_history;
-    std::vector<float> theta_history;
+    std::vector<float> x_history{float(start_x)};
+    std::vector<float> y_history{float(start_y)};
+    std::vector<float> theta_history{0};
+    std::vector<float> time{0};
+    
 
     if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0)
     {
@@ -75,6 +86,7 @@ int main(int argc, char* args[])
         bool quit = false;
         float delay;
         int x, y;
+        float a=0;
         Eigen::VectorXf state = Eigen::VectorXf::Zero(6);
 
         while (!quit)
@@ -86,12 +98,24 @@ int main(int argc, char* args[])
                 {
                     quit = true;
                 }
-                else if (e.type == SDL_MOUSEMOTION)
+                else 
                 {
-                    SDL_GetMouseState(&x, &y);
-                    std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
+                    if (e.type == SDL_MOUSEMOTION)
+                    {
+                        SDL_GetMouseState(&x, &y);
+                        std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
+                    }
+                    if(e.type == SDL_MOUSEBUTTONDOWN){
+                        SDL_GetMouseState(&x, &y);
+                        goal_state<<x,y,0,0,0,0;
+                        std::cout<<"Mouse left-click "<<x<<" "<<y<<std::endl;
+                        quadrotor.SetGoal(goal_state);
+                    }
+                    else if(e.type==SDL_KEYDOWN && e.key.keysym.sym==SDLK_p){
+                        std::thread p(doPlot,x_history,y_history,theta_history,time);
+                        p.detach();
+                    }
                 }
-                
             }
 
             SDL_Delay((int) dt * 1000);
@@ -107,6 +131,21 @@ int main(int argc, char* args[])
             /* Simulate quadrotor forward in time */
             control(quadrotor, K);
             quadrotor.Update(dt);
+            while(abs(x_history.back()-quadrotor.GetState()[0])>0.2 || abs(y_history.back()-quadrotor.GetState()[1])>0.2){
+                if(a>10000){
+                    //removes from the vector
+                     time.erase(time.begin());
+                     x_history.erase(x_history.begin());
+                     y_history.erase(y_history.begin());
+                     theta_history.erase(theta_history.begin());
+                     }
+            time.push_back(a++);
+            x_history.push_back(quadrotor.GetState()[0]);
+            y_history.push_back(quadrotor.GetState()[1]);
+            theta_history.push_back(quadrotor.GetState()[2]);
+            
+            }
+        
         }
     }
     SDL_Quit();
--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/HubertX04/Projekt_4.git@"
+#define SDL_REVISION "https://github.com/HubertX04/Projekt_4.git@b74d30c576d973d8c259db5c43e3c8392df53870"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
